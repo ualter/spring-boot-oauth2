3afce6c20f6aa0f270890f7828d6d5208c14a984
ualter <ualter.junior@gmail.com>
2018-03-23 12:23:39 +0100

    Login/Logout buttons implemented v1.5

--- security-oauth2-sso-facebook/src/main/java/com/security/oath2/sso/facebook/OauthSsoFacebook.java
+++ security-oauth2-sso-facebook/src/main/java/com/security/oath2/sso/facebook/OauthSsoFacebook.java
@@ -1,16 +1,36 @@
 package com.security.oath2.sso.facebook;
 
 import org.springframework.boot.SpringApplication;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @SpringBootApplication
 @EnableOAuth2Sso
-public class OauthSsoFacebook 
+public class OauthSsoFacebook extends WebSecurityConfigurerAdapter
 {
     public static void main( String[] args )
     {
     	SpringApplication.run(OauthSsoFacebook.class, args);
     }
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.antMatcher("/**")
+			.authorizeRequests()
+				.antMatchers("/", "/login", "/webjars/**")
+				.permitAll()
+			.anyRequest()
+				.authenticated()
+			.and()
+				.logout().logoutSuccessUrl("/").permitAll()
+			.and()
+				.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+	}
+    
+    
+    
 }
---
+++ security-oauth2-sso-facebook/src/main/java/com/security/oath2/sso/facebook/controller/OauthSsoFacebookController.java
@@ -0,0 +1,16 @@
+package com.security.oath2.sso.facebook.controller;
+
+import java.security.Principal;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OauthSsoFacebookController {
+
+	@RequestMapping("/user")
+	public Principal user(Principal principal) {
+		return principal;
+	}
+
+}

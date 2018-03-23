038105c6e1c868f4e52d4b9bfdd342e43c0b44bc
ualter <ualter.junior@gmail.com>
2018-03-23 11:58:58 +0100

    Facebook SSO v1.0

--- security-oauth2-sso-facebook/src/main/java/com/security/oath2/sso/facebook/OauthSsoFacebook.java
+++ security-oauth2-sso-facebook/src/main/java/com/security/oath2/sso/facebook/OauthSsoFacebook.java
@@ -1,6 +1,7 @@
 package com.security.oath2.sso.facebook;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 

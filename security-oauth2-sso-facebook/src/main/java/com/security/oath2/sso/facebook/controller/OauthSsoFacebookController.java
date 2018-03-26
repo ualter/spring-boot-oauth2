a32be90ea0db5b090fbd675da25f6d3381d28385
ualter <ualter.junior@gmail.com>
2018-03-26 13:26:05 +0200

    Intercept 401 Unathenticated Error

--- security-oauth2-sso-facebook/src/main/java/com/security/oath2/sso/facebook/controller/OauthSsoFacebookController.java
+++ security-oauth2-sso-facebook/src/main/java/com/security/oath2/sso/facebook/controller/OauthSsoFacebookController.java
@@ -1,33 +1,19 @@
 package com.security.oath2.sso.facebook.controller;
 
-import java.security.Principal;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@RestController
+@Controller
 public class OauthSsoFacebookController {
-
-	@Autowired
-	OAuth2ClientContext oauth2ClientContext;
-
-	@RequestMapping({ "/user" })
-	public Principal user(Principal principal) {
-		String msg = String.format("User:%s, Access_Token:%s", principal.getName(),oauth2ClientContext.getAccessToken());
-		log.info(msg);
-		return principal;
-	}
 	
-	@RequestMapping({ "/userInfo" })
-	public Principal userInfo(Principal principal) {
-		String msg = String.format("UserInfo:%s, Access_Token:%s", principal.getName(),oauth2ClientContext.getAccessToken());
-		log.info(msg);
-		return principal;
+	
+	@RequestMapping("/unauthenticated")
+	public String unauthenticated() {
+	  log.debug("/unauthenticated called!");
+	  return "redirect:/?error=true";
 	}
 
 }
---
+++ security-oauth2-sso-facebook/src/main/java/com/security/oath2/sso/facebook/controller/OauthSsoFacebookRestController.java
@@ -0,0 +1,33 @@
+package com.security.oath2.sso.facebook.controller;
+
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+public class OauthSsoFacebookRestController {
+
+	@Autowired
+	OAuth2ClientContext oauth2ClientContext;
+
+	@RequestMapping({ "/user" })
+	public Principal user(Principal principal) {
+		String msg = String.format("User:%s, Access_Token:%s", principal.getName(),oauth2ClientContext.getAccessToken());
+		log.info(msg);
+		return principal;
+	}
+	
+	@RequestMapping({ "/userInfo" })
+	public Principal userInfo(Principal principal) {
+		String msg = String.format("UserInfo:%s, Access_Token:%s", principal.getName(),oauth2ClientContext.getAccessToken());
+		log.info(msg);
+		return principal;
+	}
+
+}
